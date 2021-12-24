a095f2707e82660291d3b9f277cf8e72bc274112
unknown <jon_law98@hotmail.com>
2021-12-24 23:33:18 +0800

    Fixed image URL bug

--- layouts/default.vue
+++ layouts/default.vue
@@ -110,7 +110,7 @@ export default {
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: 'https://jonathanlawhh.com' + this.seo.image || 'https://jonathanlawhh.com/img/og/me.png',
+          content: this.seo.image ? 'https://jonathanlawhh.com' + this.seo.image : 'https://jonathanlawhh.com/img/og/me.png',
         },
         {
           hid: 'og:title',
@@ -125,7 +125,7 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
-          content: 'https://jonathanlawhh.com' + this.seo.image || 'https://jonathanlawhh.com/img/og/me.png',
+          content: this.seo.image ? 'https://jonathanlawhh.com' + this.seo.image : 'https://jonathanlawhh.com/img/og/me.png',
         },
         {
           hid: 'og:url',
